e805904601947541b27fb0ecd49469bb8039d883
Tamasru Pain <tamasrupain02@gmail.com>
2025-06-08 15:49:58 +0530

    second commit

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,16 @@
-// declare module "next-auth" {
-//     interface User {
-//         id: string;
-//     }
-//
-//     interface Session {
-//         user: User;
-//     }
-// }
+// next-auth.d.ts (in root or types folder)
+
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            /** Existing fields */
+            name?: string | null;
+            email?: string | null;
+            image?: string | null;
+
+            /** Add your custom role */
+            role?: string;
+        };
+    }
+}
