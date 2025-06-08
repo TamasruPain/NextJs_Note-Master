4c05f69a735c9b7802f68c939abb35a5c8e05edf
Tamasru Pain <tamasrupain02@gmail.com>
2025-06-08 15:11:17 +0530

    second commit

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -2,48 +2,65 @@ import connectMongoDB from "@/lib/mongodb";
 import NoteModel from "@/models/noteModel";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
+// Get a specific note
+export async function GET(
+    request: NextRequest,
+    context: { params: { id: string } }
+) {
     const { id } = context.params;
-
     await connectMongoDB();
-    await NoteModel.findByIdAndDelete(id);
 
-    return NextResponse.json({ message: 'note deleted successfully' }, { status: 200 });
-}
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-    await connectMongoDB();
-    const note = await NoteModel.findById(params.id);
+    const note = await NoteModel.findById(id);
     if (!note) {
         return NextResponse.json({ message: "Note not found" }, { status: 404 });
     }
-    const mappedNote = {
+
+    return NextResponse.json({
         id: note._id.toString(),
         title: note.title,
         content: note.content,
         createdAt: note.createdAt,
-        userId: note.userId
-    };
-    return NextResponse.json(mappedNote, { status: 200 });
+        userId: note.userId,
+    });
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-    await connectMongoDB();
+// Update a specific note
+export async function PUT(
+    request: NextRequest,
+    context: { params: { id: string } }
+) {
+    const { id } = context.params;
     const { title, content } = await request.json();
+
+    await connectMongoDB();
+
     const updatedNote = await NoteModel.findByIdAndUpdate(
-        params.id,
+        id,
         { title, content },
         { new: true }
     );
+
     if (!updatedNote) {
         return NextResponse.json({ message: "Note not found" }, { status: 404 });
     }
-    const mappedNote = {
+
+    return NextResponse.json({
         id: updatedNote._id.toString(),
         title: updatedNote.title,
         content: updatedNote.content,
         createdAt: updatedNote.createdAt,
-        userId: updatedNote.userId
-    };
-    return NextResponse.json(mappedNote);
+        userId: updatedNote.userId,
+    });
+}
+
+// Delete a specific note
+export async function DELETE(
+    request: NextRequest,
+    context: { params: { id: string } }
+) {
+    const { id } = context.params;
+    await connectMongoDB();
+    await NoteModel.findByIdAndDelete(id);
+
+    return NextResponse.json({ message: "Note deleted successfully" }, { status: 200 });
 }
